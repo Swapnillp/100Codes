31092849ee9a90f5545f6a758fde3bba46fc2fe3
Swapnillp <nilpatil917@gmail.com>
2024-08-15 19:56:00 +0530

    add function

--- P7.java
+++ P7.java
@@ -1,7 +1,31 @@
 //check prime number
 import java.util.Scanner;
 class P7{
+    public static boolean checkPrime(int no){
+        if(no<2){
+            return true;
+        }
+        if(no==2){
+            return false;
+        }
+        for(int i=2;i<no/2;i++){
+            if(no%i==0){
+               return true;
+            }
+        }
+        return false;
+   
+    }
     public static void main(String args[]){
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int no=sc.nextInt();
+        P7.checkPrime(no);
+        if(checkPrime(no)==true){
+            System.out.println("no is not prime");
+        }else{
+            System.out.println("no is prime");
+        }
+
     }
 }
