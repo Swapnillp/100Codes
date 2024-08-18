69bcde787d237308a05c0a44f5ff494e4edfec4b
Swapnillp <nilpatil917@gmail.com>
2024-08-18 17:13:35 +0530

    Linear Search

---
+++ P17.java
@@ -0,0 +1,22 @@
+//Linear Search
+class P17{
+
+    public static int LinearSearch(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6,7};
+        int key=5;
+        int index=LinearSearch(arr,key);
+        if (index==-1){
+            System.out.println("key not found");
+        }else{
+        System.out.println("index of arry is :"+index);
+        }
+    }
+}
