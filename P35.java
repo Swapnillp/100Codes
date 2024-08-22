a957218c9455958128e80f3f3c1de44bb7bb1920
Swapnillp <nilpatil917@gmail.com>
2024-08-22 16:24:10 +0530

    basic string program

---
+++ P35.java
@@ -0,0 +1,12 @@
+//String 
+//String are java in immutable
+class P35{
+    public static void main(String args[]){
+        //declaraion
+        String str=new String("nil");
+        //dec 2
+        String str1="swapnil";
+        System.out.println(str);
+        System.out.println(str1);
+    }
+}
