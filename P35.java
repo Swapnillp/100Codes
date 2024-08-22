13729624af2963b517a986c3faaa0764a98553bb
Swapnillp <nilpatil917@gmail.com>
2024-08-22 16:31:53 +0530

    use nextLine fun

--- P35.java
+++ P35.java
@@ -1,5 +1,6 @@
 //String 
 //String are java in immutable
+import java.util.Scanner;
 class P35{
     public static void main(String args[]){
         //declaraion
@@ -8,5 +9,9 @@ class P35{
         String str1="swapnil";
         System.out.println(str);
         System.out.println(str1);
+        //by input
+        Scanner sc=new Scanner(System.in);
+        String str2=sc.nextLine();
+        System.out.println(str2);
     }
 }
