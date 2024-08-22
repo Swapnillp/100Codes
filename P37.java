b22b5dfacd3b526f305888b5cef85f60d610ab3d
Swapnillp <nilpatil917@gmail.com>
2024-08-22 19:48:12 +0530

    string compare

---
+++ P37.java
@@ -0,0 +1,14 @@
+//string compare
+class P37{
+    public static void main(String args[]){
+        String str="nil";
+        String str1="nil";
+        String str2=new String("nil");
+        if(str==str1){
+            System.out.println("string is equal");
+        }
+        if(str1.equals(str2)){
+            System.out.println("equal");
+        }
+    }
+}
