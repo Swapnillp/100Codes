b46641ce6a1d3295485a4034072c0f4f23d113a8
Swapnillp <nilpatil917@gmail.com>
2024-08-24 19:33:20 +0530

    StringBuilder program

---
+++ P39.java
@@ -0,0 +1,9 @@
+//stringbuilder
+class P39{
+    public static void main(String args[]){
+        StringBuilder sb=new StringBuilder("Swapnil");
+        System.out.println(sb);
+        //append
+        System.out.println(sb.append(" Patil"));
+    }
+}
